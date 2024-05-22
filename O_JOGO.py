689503fc5026acd03b6919b73f60f1d364b7701d
paulaehrenberger <paulafe@al.insper.edu.br>
2024-05-22 12:02:28 -0300

    Todos os nívies

--- O_JOGO.py
+++ O_JOGO.py
@@ -3,7 +3,7 @@ import random
 import time
 from inicio import*
 from Fase1 import* 
-# from Fase2 import*
-# from tela_prox_nivel import*
+from Fase2 import*
+from tela_prox_nivel import*
 from Fase3 import*
 
