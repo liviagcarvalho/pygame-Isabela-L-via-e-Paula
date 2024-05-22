8b2287943318d27e307b6c0dd31f904b50b56df9
paulaehrenberger <paulafe@al.insper.edu.br>
2024-05-22 11:37:10 -0300

    pronto

--- O_JOGO.py
+++ O_JOGO.py
@@ -1,10 +1,10 @@
 import pygame
 import random
 import time
-
-# from inicio import*
+from inicio import*
 from Fase1 import* 
 from Fase2 import*
+from tela_prox_nivel import*
 from Fase3 import*
 from salvou_reino import*
 
